fd80b2c40d6239264aa342d61172e449b065d8c8
Kshitij2011 <77619958+Kshitij2011@users.noreply.github.com>
2021-10-28 16:57:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-img = "";
+
 status = "";
 objects = [];
+song = "";
+
+function preload(){
+    song = loadSound("pleasent_flute.mp3");
+ }
 
 function setup(){
     canvas = createCanvas(380, 380);
@@ -29,9 +34,7 @@ function gotResult(error, results){
     objects = results;
 }
 
-function preload(){
-       
-}
+
 
 function draw(){
     image(video, 0, 0, 380, 380);
@@ -48,7 +51,6 @@ function draw(){
   for (i = 0; i < objects.length; i++)
   {
       document.getElementById("status").innerHTML = "Status : Object Detected";
-      document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :"+objects.length;
 
       fill(r,g,b);
       percent = floor(objects[i].confidence * 100);
@@ -56,6 +58,25 @@ function draw(){
       noFill();
       stroke(r,g,b);
       rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height );
+
+if(objects[i].label=="person")
+{document.getElementById("number_of_objects").innerHTML = "Baby Found";
+console.log("stop");
+song.stop();
+}
+
+else{
+    document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+console.log("play");
+song.play();
+}
+
   }
+
+  if(objects.length == 0)
+   { document.getElementById("number_of_objects").innerHTML = "Baby Not Found";
+    console.log("play"); song.play();
+    
+ }
     }
 }
